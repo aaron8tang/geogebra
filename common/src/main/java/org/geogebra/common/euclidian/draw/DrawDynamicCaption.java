b87854572e2e19fe5454911f4ed21b95858bb82e
Laszlo Gal <laszlo@geogebra.org>
2020-07-21 15:44:08 +0200

    WLY-108 highlighting label, setting visual style

--- common/src/main/java/org/geogebra/common/euclidian/draw/DrawDynamicCaption.java
+++ common/src/main/java/org/geogebra/common/euclidian/draw/DrawDynamicCaption.java
@@ -1,6 +1,8 @@
 package org.geogebra.common.euclidian.draw;
 
+import org.geogebra.common.awt.GColor;
 import org.geogebra.common.awt.GGraphics2D;
+import org.geogebra.common.awt.GRectangle;
 import org.geogebra.common.euclidian.EuclidianView;
 import org.geogebra.common.kernel.geos.GeoInputBox;
 import org.geogebra.common.kernel.geos.GeoText;
@@ -27,6 +29,7 @@ public class DrawDynamicCaption {
 	}
 
 	void draw(GGraphics2D g2) {
+		positionDynamicCaption();
 		drawTextCaption.draw(g2);
 	}
 
@@ -36,12 +39,13 @@ public class DrawDynamicCaption {
 		}
 
 		updateDrawTextCaption();
-		positionDynamicCaption();
-		positionDynamicCaption();
+		measureCaption();
 	}
 
 	private void updateDrawTextCaption() {
 		GeoText caption = captionText.copy();
+		caption.setAllVisualPropertiesExceptEuclidianVisible(captionText,
+				false, false);
 		caption.setFontSizeMultiplier(inputBox.getFontSizeMultiplier());
 		caption.setEuclidianVisible(true);
 		caption.setAbsoluteScreenLocActive(true);
@@ -55,14 +59,25 @@ public class DrawDynamicCaption {
 
 		captionWidth = (int) drawTextCaption.getBounds().getWidth();
 		captionHeight = (int) drawTextCaption.getBounds().getHeight();
+		drawInputBox.labelSize.x = captionWidth;
+		drawInputBox.labelSize.y = captionHeight;
 		drawInputBox.calculateBoxBounds();
 		return captionText.isLaTeX();
 	}
 
 	private void positionDynamicCaption() {
-		int x = drawInputBox.xLabel ;
-		int y = drawInputBox.yLabel;
-		drawTextCaption.xLabel = x - captionWidth;
-		drawTextCaption.yLabel = y - captionHeight / 2;
+		drawTextCaption.xLabel = drawInputBox.xLabel - captionWidth;
+		int middle = drawInputBox.boxTop + drawInputBox.boxHeight / 2;
+		drawTextCaption.yLabel = middle - captionHeight / 2;
+	}
+
+	public void highlightLabel(GGraphics2D g2) {
+		if (!drawInputBox.isHighlighted()) {
+			return;
+		}
+		g2.setPaint(GColor.LIGHT_GRAY);
+		GRectangle bounds = drawTextCaption.getBounds();
+		g2.fillRect((int)bounds.getX(), (int)bounds.getY(), (int)bounds.getWidth(),
+				(int)bounds.getHeight());
 	}
 }
